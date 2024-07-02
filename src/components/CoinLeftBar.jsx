5756fce9f13e550e9488deabc059716f6d3cd04a
Kunj05 <kunjchandak05@gmail.com>
2024-07-02 19:52:35 +0530

    new library used

--- src/components/CoinLeftBar.jsx
+++ src/components/CoinLeftBar.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { SingleCoin } from '../Api';
-import ReactHtmlParser from "react-html-parser";
+import DOMPurify from 'dompurify';
 import { useSelector } from 'react-redux';
 import { selectuser } from '../redux/userSlice';
 import { doc, setDoc } from 'firebase/firestore';
@@ -71,7 +71,9 @@ const CoinLeftBar = () => {
                 <h3 className='p-2 pb-0 text-5xl font-bold'>{coin?.name}</h3>
             </div>
             <div className='w-full flex flex-col items-start'>
-                <h6 className='p-2 pb-0 text-sm'>{ReactHtmlParser(coin?.description.en.split(". ")[0])}.</h6>
+                <h6 className='p-2 pb-0 text-sm'>          {coin?.description?.en ? (
+            <span dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(coin.description.en.split(". ")[0]) }} />
+          ) : "No description available"}.</h6>
                 <h5 className='p-2 pb-0 text-lg flex justify-start'>Rank: {coin?.market_cap_rank}</h5>
                 <h5 className='p-2 pb-0 text-lg flex justify-start'>Current Price: {currency === 'usd' ? "$" : "₹"} {coin?.market_data.current_price[currency].toLocaleString()}</h5>
                 <h5 className='p-2  text-lg flex justify-start'>Market Cap: {currency === 'usd' ? "$" : "₹"} {Number((coin?.market_data.market_cap[currency] / 1000000).toFixed(0)).toLocaleString()} M</h5>
